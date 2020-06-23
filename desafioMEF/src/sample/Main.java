fe4a44096aced744f8517fc97d18e525f7700c4a
MarleneBarahona <00118616@uca.edu.sv>
2020-06-22 23:10:44 -0600

    Mejoras visuales, agregando background al menu y modificando distribucion de botones en ventana principal.

--- desafioMEF/src/sample/Main.java
+++ desafioMEF/src/sample/Main.java
@@ -9,8 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -25,7 +24,9 @@ public class Main extends Application {
         //Menu
         Stage menuStage = new Stage();
         windowMenu = menuStage;
-        Button buttonIniciar = new Button("Iniciar mierda");
+        Button buttonInicio = new Button("Ventana Inicio");
+        buttonInicio.setOnAction(event -> window.setScene(sceneInicio));
+        Button buttonIniciar = new Button("Iniciar");
         buttonIniciar.setOnAction(event -> window.setScene(scene1));
         Button button1 = new Button("Dominio");
         button1.setOnAction(event -> window.setScene(scene1));
@@ -36,29 +37,33 @@ public class Main extends Application {
         Button button4 = new Button("MEF");
         button4.setOnAction(event -> window.setScene(scene4));
         VBox layoutMenu = new VBox(10);
-        layoutMenu.getChildren().addAll(buttonIniciar,button1,button2,button3,button4);
-        layoutMenu.setPadding(new Insets(20,20,10,20));
+        Image imageMenu = new Image(getClass().getResourceAsStream("images/menu.png"));
+        // new BackgroundSize(width, height, widthAsPercentage, heightAsPercentage, contain, cover)
+        BackgroundSize backgroundSize = new BackgroundSize(400, 400, true, true, true, false);
+        // new BackgroundImage(image, repeatX, repeatY, position, size)
+        BackgroundImage backgroundImage = new BackgroundImage(imageMenu, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, backgroundSize);
+        // new Background(images...)
+        Background background = new Background(backgroundImage);
+        layoutMenu.getChildren().addAll(buttonInicio, buttonIniciar,button1,button2,button3,button4);
+        layoutMenu.setPadding(new Insets(20,20,0,20));
+        layoutMenu.setBackground(new Background(backgroundImage));
         sceneMenu = new Scene(layoutMenu,200,300);
+
         windowMenu.setTitle("Menu");
         windowMenu.setX(70);
         windowMenu.setY(85);
         windowMenu.setScene(sceneMenu);
-        //windowMenu.show();
 
-        //principal
-        //Button buttonIniciar = new Button("Iniciar mierda");
-        //buttonIniciar.setOnAction(event -> window.setScene(scene1));
+        //Principal
         Image imageInicio = new Image(getClass().getResourceAsStream("images/imagenInicio.png"));
         ImageView imageViewInicio = new ImageView(imageInicio);
-        imageViewInicio.setFitHeight(450);
+        imageViewInicio.setFitHeight(400);
         imageViewInicio.setFitWidth(800);
-        VBox layoutInicio = new VBox(20);
-        layoutInicio.getChildren().addAll(imageViewInicio);
-        //layoutInicio.getChildren().addAll(imageViewInicio,buttonIniciar);
+        VBox layoutInicio = new VBox(imageViewInicio);
+        //layoutInicio.getChildren().addAll(imageViewInicio);
         sceneInicio = new Scene(layoutInicio, 800, 500);
 
-        //ventana 1
-        Label label1 = new Label("Dominio");
+        //ventana 1 - Dominio
         Button buttonAnt1 = new Button("Anterior");
         buttonAnt1.setOnAction(event -> window.setScene(sceneInicio));
         Button buttonSig1 = new Button("Siguiente");
@@ -66,13 +71,13 @@ public class Main extends Application {
         Image image1 = new Image(getClass().getResourceAsStream("images/Dominio.png"));
         ImageView imageView1 = new ImageView(image1);
         imageView1.setFitHeight(400);
-        imageView1.setFitWidth(750);
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1,imageView1,buttonAnt1,buttonSig1);
+        imageView1.setFitWidth(800);
+        HBox h1 = new HBox(670);
+        h1.getChildren().addAll(buttonAnt1,buttonSig1);
+        VBox layout1 = new VBox(imageView1,h1);
         scene1 = new Scene(layout1, 800, 500);
 
-        //ventana 2
-        Label label2 = new Label("Mallado");
+        //ventana 2 - Mallado
         Button buttonAnt2 = new Button("Anterior");
         buttonAnt2.setOnAction(event -> window.setScene(scene1));
         Button buttonSig2 = new Button("Siguiente");
@@ -80,41 +85,37 @@ public class Main extends Application {
         Image image2 = new Image(getClass().getResourceAsStream("images/Mallado.png"));
         ImageView imageView2 = new ImageView(image2);
         imageView2.setFitHeight(400);
-        imageView2.setFitWidth(750);
-        VBox layout2 = new VBox(20);
-        layout2.getChildren().addAll(label2,imageView2,buttonAnt2,buttonSig2);
+        imageView2.setFitWidth(800);
+        HBox h2 = new HBox(670);
+        h2.getChildren().addAll(buttonAnt2,buttonSig2);
+        VBox layout2 = new VBox(imageView2,h2);
         scene2 = new Scene(layout2, 800, 500);
 
-        //ventana 3
-        Label label3 = new Label("Modelo");
+        //ventana 3 - Modelo
         Button buttonAnt3 = new Button("Anterior");
         buttonAnt3.setOnAction(event -> window.setScene(scene2));
-        //Button volverInicio = new Button("Volver Inicio");
-        //volverInicio.setOnAction(event -> window.setScene(sceneInicio));
         Button buttonSig3 = new Button("Siguiente");
         buttonSig3.setOnAction(event -> window.setScene(scene4));
         Image image3 = new Image(getClass().getResourceAsStream("images/Modelo.png"));
         ImageView imageView3 = new ImageView(image3);
         imageView3.setFitHeight(400);
-        imageView3.setFitWidth(750);
-        VBox layout3 = new VBox(20);
-        layout3.getChildren().addAll(label3,imageView3,buttonAnt3,buttonSig3);
+        imageView3.setFitWidth(800);
+        HBox h3 = new HBox(670);
+        h3.getChildren().addAll(buttonAnt3,buttonSig3);
+        VBox layout3 = new VBox(imageView3,h3);
         scene3 = new Scene(layout3, 800, 500);
 
-        //ventana 4
-        Label label4 = new Label("MEF");
+        //ventana 4 - MEF
         Button buttonAnt4 = new Button("Anterior");
-        buttonAnt3.setOnAction(event -> window.setScene(scene3));
-        //Button volverInicio = new Button("Volver Inicio");
-        //volverInicio.setOnAction(event -> window.setScene(sceneInicio));
-        //Button buttonSig3 = new Button("Siguiente mierda");
-        //buttonSig3.setOnAction(event -> window.setScene(scene3));
+        buttonAnt4.setOnAction(event -> window.setScene(scene3));
+        Button buttonSig4 = new Button("Siguiente");
         Image image4 = new Image(getClass().getResourceAsStream("images/MEF.png"));
         ImageView imageView4 = new ImageView(image4);
         imageView4.setFitHeight(400);
-        imageView4.setFitWidth(750);
-        VBox layout4 = new VBox(20);
-        layout4.getChildren().addAll(label4,imageView4,buttonAnt4);
+        imageView4.setFitWidth(800);
+        HBox h4 = new HBox(670);
+        h4.getChildren().addAll(buttonAnt4,buttonSig4);
+        VBox layout4 = new VBox(imageView4,h4);
         scene4 = new Scene(layout4, 800, 500);
 
         window.setScene(sceneInicio);
