4f6a7ca1dbf43339d39373daca66bfbc3123982a
MarleneBarahona <00118616@uca.edu.sv>
2020-07-01 21:31:31 -0600

    Agregando video final del ensamblaje y una imagen adicional a dicha pantalla. Modificando dimensiones de la ventana y cambiando la ventana de despedida.

--- desafioMEF/src/sample/Main.java
+++ desafioMEF/src/sample/Main.java
@@ -20,8 +20,8 @@ import javafx.util.Duration;
 
 public class Main extends Application {
 
-    Stage window, windowMenu, windowPlayer;
-    Scene sceneInicio, sceneMenu, scene1, scene2, scene3, scene4, scene5, scene6, scene7;
+    Stage window, windowMenu, windowIdk;
+    Scene sceneInicio, sceneMenu, scene1, scene2, scene3, scene4, scene5, scene6, scene7, sceneFinal;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -54,11 +54,8 @@ public class Main extends Application {
         Button button7 = new Button("Condiciones de contorno");
         button7.setPrefSize(150,5);
         button7.setOnAction(event -> window.setScene(scene7));
-       // Button buttonIniciar = new Button("Fin");
-        //buttonIniciar.setPrefSize(150,5);
-        //buttonIniciar.setOnAction(event -> window.setScene(scene1));
         VBox layoutMenu = new VBox(8);
-        Image imageMenu = new Image(getClass().getResourceAsStream("images/menu3.png"));
+        Image imageMenu = new Image(getClass().getResourceAsStream("/sample/images/menu3.png"));
         // new BackgroundSize(width, height, widthAsPercentage, heightAsPercentage, contain, cover)
         BackgroundSize backgroundSize = new BackgroundSize(400, 400, true, true, true, false);
         // new BackgroundImage(image, repeatX, repeatY, position, size)
@@ -77,71 +74,55 @@ public class Main extends Application {
         windowMenu.setScene(sceneMenu);
 
         //Principal
-        Image imageInicio = new Image(getClass().getResourceAsStream("images/Diapositiva1.png"));
+        Image imageInicio = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva1.png"));
         ImageView imageViewInicio = new ImageView(imageInicio);
         imageViewInicio.setFitHeight(400);
         imageViewInicio.setFitWidth(800);
 
-        //Button btn = new Button("ClickMe");
-        //Group group = new Group(btn);
-        //Scene scene = new Scene(group, 600, 600);
         VBox layoutInicio = new VBox(imageViewInicio);
-        sceneInicio = new Scene(layoutInicio, 800, 500);
-        //Duration = 2.5 seconds
-        /*Duration duration = Duration.millis(2500);
-        //Create new translate transition
-        TranslateTransition transition = new TranslateTransition(duration, imageViewInicio);
-        //Move in X axis by +200
-        //transition.setByX(0);
-        //Move in Y axis by +100
-        transition.setByY(100);
-        //Go back to previous position after 2.5 seconds
-        transition.setAutoReverse(true);
-        //Repeat animation twice
-        transition.setCycleCount(2);
-        transition.play();*/
+        sceneInicio = new Scene(layoutInicio, 800, 460);
 
         //ventana 1 - Dominio
         Button buttonAnt1 = new Button("Anterior");
         buttonAnt1.setOnAction(event -> window.setScene(sceneInicio));
         Button buttonSig1 = new Button("Siguiente");
         buttonSig1.setOnAction(event -> window.setScene(scene2));
-        Image image1 = new Image(getClass().getResourceAsStream("images/Diapositiva2.png"));
+        Image image1 = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva2.png"));
         ImageView imageView1 = new ImageView(image1);
         imageView1.setFitHeight(400);
         imageView1.setFitWidth(800);
         HBox h1 = new HBox(670);
         h1.getChildren().addAll(buttonAnt1,buttonSig1);
         VBox layout1 = new VBox(imageView1,h1);
-        scene1 = new Scene(layout1, 800, 500);
+        scene1 = new Scene(layout1, 800, 460);
 
         //ventana 2 - Mallado
         Button buttonAnt2 = new Button("Anterior");
         buttonAnt2.setOnAction(event -> window.setScene(scene1));
         Button buttonSig2 = new Button("Siguiente");
         buttonSig2.setOnAction(event -> window.setScene(scene3));
-        Image image2 = new Image(getClass().getResourceAsStream("images/Diapositiva3.png"));
+        Image image2 = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva3.png"));
         ImageView imageView2 = new ImageView(image2);
         imageView2.setFitHeight(400);
         imageView2.setFitWidth(800);
         HBox h2 = new HBox(670);
         h2.getChildren().addAll(buttonAnt2,buttonSig2);
         VBox layout2 = new VBox(imageView2,h2);
-        scene2 = new Scene(layout2, 800, 500);
+        scene2 = new Scene(layout2, 800, 460);
 
         //ventana 3 - Modelo
         Button buttonAnt3 = new Button("Anterior");
         buttonAnt3.setOnAction(event -> window.setScene(scene2));
         Button buttonSig3 = new Button("Siguiente");
         buttonSig3.setOnAction(event -> window.setScene(scene4));
-        Image image3 = new Image(getClass().getResourceAsStream("images/Diapositiva4.png"));
+        Image image3 = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva4.png"));
         ImageView imageView3 = new ImageView(image3);
         imageView3.setFitHeight(400);
         imageView3.setFitWidth(800);
         HBox h3 = new HBox(670);
         h3.getChildren().addAll(buttonAnt3,buttonSig3);
         VBox layout3 = new VBox(imageView3,h3);
-        scene3 = new Scene(layout3, 800, 500);
+        scene3 = new Scene(layout3, 800, 460);
 
         //ventana 4 - MEF
         Button buttonAnt4 = new Button("Anterior");
@@ -157,14 +138,14 @@ public class Main extends Application {
         Button buttonPaso6 = new Button("6");
         Button buttonWeak = new Button("W");
         //Cambiar entre los pasos
-        Image imagePaso1 = new Image(getClass().getResourceAsStream("images/Diapositiva5.png"));
-        Image imagePaso2 = new Image(getClass().getResourceAsStream("images/Diapositiva6.png"));
-        Image imagePaso3 = new Image(getClass().getResourceAsStream("images/Diapositiva7.png"));
-        Image imagePaso4 = new Image(getClass().getResourceAsStream("images/Diapositiva8.png"));
-        Image imagePaso5 = new Image(getClass().getResourceAsStream("images/Diapositiva9.png"));
-        Image imagePaso6 = new Image(getClass().getResourceAsStream("images/Diapositiva11.png"));
-        Image imageStrong = new Image(getClass().getResourceAsStream("images/Diapositiva10.png"));
-        Image imageWeak = new Image(getClass().getResourceAsStream("images/Diapositiva12.png"));
+        Image imagePaso1 = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva5.png"));
+        Image imagePaso2 = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva6.png"));
+        Image imagePaso3 = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva7.png"));
+        Image imagePaso4 = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva8.png"));
+        Image imagePaso5 = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva9.png"));
+        Image imagePaso6 = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva11.png"));
+        Image imageStrong = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva10.png"));
+        Image imageWeak = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva12.png"));
         ImageView imageView4 = new ImageView(imagePaso1);
         imageView4.setFitHeight(400);
         imageView4.setFitWidth(800);
@@ -179,13 +160,13 @@ public class Main extends Application {
         buttonStrong.setOnAction(event -> imageView4.setImage(imageStrong));
         buttonWeak.setOnAction(event -> imageView4.setImage(imageWeak));
         VBox layout4 = new VBox(imageView4,h4);
-        scene4 = new Scene(layout4, 800, 500);
+        scene4 = new Scene(layout4, 800, 460);
 
         //ventana 5 - Def componentes
         Button buttonAnt5 = new Button("Anterior");
-        buttonAnt4.setOnAction(event -> window.setScene(scene3));
+        buttonAnt4.setOnAction(event -> window.setScene(scene4));
         Button buttonSig5 = new Button("Siguiente");
-        buttonSig5.setOnAction(event -> window.setScene(scene7));
+        buttonSig5.setOnAction(event -> window.setScene(scene6));
         Button buttonComp1 = new Button("1");
         Button buttonComp2 = new Button("2");
         Button buttonC = new Button("C");
@@ -194,13 +175,13 @@ public class Main extends Application {
         Button buttonM = new Button("M");
         Button buttonGyQ = new Button("G y Q");
         //Cambiar entre los componentes
-        Image imageComp1 = new Image(getClass().getResourceAsStream("images/Diapositiva13.png"));
-        Image imageComp2 = new Image(getClass().getResourceAsStream("images/Diapositiva14.png"));
-        Image imageC = new Image(getClass().getResourceAsStream("images/Diapositiva15.png"));
-        Image imageK = new Image(getClass().getResourceAsStream("images/Diapositiva16.png"));
-        Image imageF = new Image(getClass().getResourceAsStream("images/Diapositiva17.png"));
-        Image imageM = new Image(getClass().getResourceAsStream("images/Diapositiva18.png"));
-        Image imageGyQ = new Image(getClass().getResourceAsStream("images/Diapositiva19.png"));
+        Image imageComp1 = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva13.png"));
+        Image imageComp2 = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva14.png"));
+        Image imageC = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva15.png"));
+        Image imageK = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva16.png"));
+        Image imageF = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva17.png"));
+        Image imageM = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva18.png"));
+        Image imageGyQ = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva19.png"));
         ImageView imageView5 = new ImageView(imageComp1);
         imageView5.setFitHeight(400);
         imageView5.setFitWidth(800);
@@ -214,54 +195,64 @@ public class Main extends Application {
         buttonM.setOnAction(event -> imageView5.setImage(imageM));
         buttonGyQ.setOnAction(event -> imageView5.setImage(imageGyQ));
         VBox layout5 = new VBox(imageView5,h5);
-        scene5 = new Scene(layout5, 800, 500);
+        scene5 = new Scene(layout5, 800, 460);
 
         //Ensamblaje
         Button buttonAnt6=new Button("Anterior");
         buttonAnt6.setOnAction(event -> window.setScene(scene5));
         Button buttonSig6 = new Button("Siguiente");
         buttonSig6.setOnAction(event -> window.setScene(scene7));
-        Media media = new Media(getClass().getResource("/sample/images/Ensamblaje.mp4").toExternalForm());
+        Media media = new Media(getClass().getResource("/sample/videos/Ensamblaje3.mp4").toExternalForm());
         //Instantiating MediaPlayer class
         MediaPlayer mediaPlayer = new MediaPlayer(media);
         //Instantiating MediaView class
         MediaView mediaView = new MediaView(mediaPlayer);
         //by setting this property to true, the Video will be played
         Button playEnsamblaje = new Button("Play");
-        playEnsamblaje.setOnAction(event -> mediaPlayer.setAutoPlay(true));
+        playEnsamblaje.setOnAction(event ->  mediaPlayer.setAutoPlay(true));
+        
         //mediaPlayer.setAutoPlay(true);
         mediaView.setFitHeight(400);
-        mediaView.setFitWidth(800);
-        HBox h6 = new HBox(150);
+        mediaView.setFitWidth(755);
+        Image imageEnsamblaje = new Image(getClass().getResourceAsStream("/sample/images/ensamblaje.png"));
+        ImageView imageViewEnsamblaje = new ImageView(imageEnsamblaje);
+        imageViewEnsamblaje.setFitHeight(400);
+        imageViewEnsamblaje.setFitWidth(325);
+        HBox hE = new HBox(imageViewEnsamblaje,mediaView);
+        HBox h6 = new HBox(250);
         h6.getChildren().addAll(buttonAnt6,playEnsamblaje,buttonSig6);
-        VBox layout6 = new VBox(mediaView,h6);
-        scene6 = new Scene(layout6, 800, 500);
+        VBox layout6 = new VBox(hE,h6);
+        scene6 = new Scene(layout6, 1040, 460);
 
         //Condiciones de contorno
         Button buttonAnt7 = new Button("Anterior");
         buttonAnt7.setOnAction(event -> window.setScene(scene6));
         Button buttonSig7 = new Button("Finalizar");
-        Image image7 = new Image(getClass().getResourceAsStream("images/Diapositiva20.png"));
+        Image image7 = new Image(getClass().getResourceAsStream("/sample/images/Diapositiva20.png"));
         ImageView imageView7 = new ImageView(image7);
         imageView7.setFitHeight(400);
         imageView7.setFitWidth(800);
-        buttonSig7.setOnAction(event -> imageView7.setImage(imageInicio));
+        buttonSig7.setOnAction(event -> window.setScene(sceneFinal));
         HBox h7 = new HBox(670);
         h7.getChildren().addAll(buttonAnt7,buttonSig7);
         VBox layout7 = new VBox(imageView7,h7);
-        scene7 = new Scene(layout7, 800, 500);
+        scene7 = new Scene(layout7, 800, 460);
+
+        //Final
+        Image imageFinal = new Image(getClass().getResourceAsStream("/sample/images/Gracias.png"));
+        ImageView imageViewFinal = new ImageView(imageFinal);
+        imageViewFinal.setFitHeight(400);
+        imageViewFinal.setFitWidth(800);
+
+        VBox layoutFinal = new VBox(imageViewFinal);
+        sceneFinal = new Scene(layoutFinal, 800, 460);
 
         window.setScene(sceneInicio);
-        window.setTitle("Mierda esta :)");
+        window.setTitle("Desafio de Programacion - Aplicacion MEF - Marlene Barahona");
         window.setY(85);
         window.setX(275);
         window.show();
         windowMenu.show();
-        //player.play();
-        /*Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();*/
     }
 
 
